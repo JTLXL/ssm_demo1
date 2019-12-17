0088b1443258ebf7ad131e6733b81d32edae17bb
JTLXL <1096060513@qq.com>
2019-12-17 16:21:04 +0800

    spring整合了springmvc框架

--- src/main/java/com/jtl/controller/AccountController.java
+++ src/main/java/com/jtl/controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.jtl.controller;
 
+import com.jtl.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,9 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/account")
 public class AccountController {
+
+    @Autowired
+    private AccountService as;
+
     @RequestMapping("/findAll")
     public String findAll() {
         System.out.println("表现层：查询所有账户...");
+        // 调用service的方法
+        as.findAll();
         return "list";
     }
 }
